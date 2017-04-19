853277b26ca16178e01f56f9d66dda6fd395c2e9
Alimachaves <quintero.cami@gmail.com>
2017-04-19 16:09:54 +0200

    home y shiseido

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,9 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
 }
