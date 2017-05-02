c93c0776d91145247bdb368e2f04d956a49bd490
Alimachaves <quintero.cami@gmail.com>
2017-05-02 18:30:23 +0200

    Updates

--- js/menu.js
+++ js/menu.js
@@ -7,3 +7,13 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
+
+
+
+$(document).ready(function() {
+  $('.html').css('width', '30%');
+  $('.adobe').css('width', '80%');
+  $('.wordpress').css('width', '30%');
+  $('.ui').css('width', '98%');
+  $('.rwd').css('width', '30%');
+});
